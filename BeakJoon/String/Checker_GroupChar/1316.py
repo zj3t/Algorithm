eb5252a6c65e9e4609ea309d89fadd241eeaa74f
은태  장 <zj3t@euntaeui-MacBook-Pro.local>
2020-07-07 00:28:47 +0900

    1316.py --> python solve

---
+++ BeakJoon/String/Checker_GroupChar/1316.py
@@ -0,0 +1,62 @@
+# -*- conding: utf-8 -*-
+
+'''
+https://www.acmicpc.net/problem/1316
+'''
+import time
+
+def word_check(word):
+    isCorrect = 0
+    i = -1
+    if len(word) is 1:
+        isCorrect = 1
+        return isCorrect
+    else:
+        while i+1 < len(word):
+            i = i+1
+            check_list = []
+            tmp = word[i]
+            for j in range(i,len(word)):
+                index = word.find(tmp)
+
+                if index is -1:
+                    isCorrect = 1
+                    continue
+                s = list(word)
+                s[index] = '!'
+                
+                word = "".join(s)
+
+                check_list.append(index)
+
+            check_list = [x for x in check_list if x>=0]
+
+            if len(check_list) == 1:
+                isCorrect = 1
+                continue
+            else:
+                for h in range(0, len(check_list)-1):
+                    if check_list[h] + 1 is check_list[h+1]:
+                        isCorrect = 1
+                        i=i+1
+                        continue
+                    else:
+                        isCorrect = 0
+                        return isCorrect
+
+    return isCorrect
+
+
+if __name__ == "__main__":
+    count = input()
+    sentence_list = []
+    number = 0
+
+    if count <= 100 and count > 0:
+        for i in range(0, count):
+            sentence_list.append(raw_input(''))
+
+        for i in range(0, len(sentence_list)): #One word
+            number += word_check(sentence_list[i])
+    
+    print number
