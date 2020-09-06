df4a4d02ee852132ddf24651f5d1ffc182b2bc3e
은태  장 <zj3t@euntaeui-MacBook-Pro.local>
2020-09-06 23:41:21 +0900

    folder

---
+++ BeakJoon/Ranking/10039.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    won = 0
+    sae = 0
+    sang = 0
+    sung = 0
+    gang = 0
+
+    won = input()
+    sae = input()
+    sang = input()
+    sung = input()
+    gang = input()
+
+    print sae
